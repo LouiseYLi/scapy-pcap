23d0014dfe3496d76763764cefc6a2c7d54bb4b2
LouiseYLi <louiseli.van@gmail.com>
2025-09-27 16:23:01 -0700

    implemented arp header parser

--- source/packet_parsers.py
+++ source/packet_parsers.py
@@ -21,9 +21,51 @@ def parse_ethernet_header(hex_data):
     return ether_type, payload
 
 
+def format_mac(mac_hex):
+    return ":".join(mac_hex[i:i+2] for i in range(0, 12, 2))
+
+def format_ip(ip_hex):
+    ip_parts = []
+    for i in range(0, 8, 2):
+        hex_byte = ip_hex[i:i+2]
+        decimal_octet = str(int(hex_byte, 16))
+        ip_parts.append(decimal_octet)
+    return ".".join(ip_parts)
+
 # Parse ARP header
 def parse_arp_header(hex_data):
     hardware_type = int(hex_data[:4], 16)
+    protocol_type = int(hex_data[4:8], 16)
+
+    hardware_length = int(hex_data[8:10], 16)
+    protocol_length = int(hex_data[10:12], 16)
+    operation = int(hex_data[12:16], 16)
+
+    sender_hardware_address_hex = hex_data[16:28]
+    sender_hardware_address = format_mac(sender_hardware_address_hex)
+    sender_protocol_address_hex = hex_data[28:36]
+    sender_protocol_address = format_ip(sender_protocol_address_hex)
+
+    target_hardware_address_hex = hex_data[36:48]
+    target_hardware_address = format_mac(target_hardware_address_hex)
+    target_protocol_address_hex = hex_data[48:56]
+    target_protocol_address = format_ip(target_protocol_address_hex)
 
     print(f"ARP Header:")
     print(f"  {'Hardware Type:':<25} {hex_data[:4]:<20} | {hardware_type}")
+    print(f"  {'Protocol Type:':<25} {hex_data[4:8]:<20} | {protocol_type}")
+
+    print(f"  {'Hardware Length:':<25} {hex_data[8:10]:<20} | {hardware_length}")
+    print(f"  {'Protocol Length:':<25} {hex_data[10:12]:<20} | {protocol_length}")
+    print(f"  {'Operation:':<25} {hex_data[12:16]:<20} | {operation}")
+
+    print(f"  {'Sender Hardware Address:':<25} {hex_data[16:28]:<20} | {sender_hardware_address}")
+    print(f"  {'Sender Protocol Address:':<25} {hex_data[28:36]:<20} | {sender_protocol_address}")
+
+    print(f"  {'Target Hardware Address:':<25} {hex_data[36:48]:<20} | {target_hardware_address}")
+    print(f"  {'Target Protocol Address:':<25} {hex_data[48:56]:<20} | {target_protocol_address}")
+
+    # print(f"\nhex stream:{hex_data}\n")
+    # print(f"\ntest:{protocol_type}\n")
+
+
